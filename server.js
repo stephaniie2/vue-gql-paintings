939f5d6a3a7ce04240964bf045698da2678ab7c7
Stephanie <stephaniiehallberg@gmail.com>
2019-06-20 20:45:02 +0200

    Setup and Mutation signupUser

---
+++ server.js
@@ -0,0 +1,32 @@
+const { ApolloServer } = require('apollo-server');
+const mongoose = require('mongoose');
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, 'typeDefs.gql');
+const typeDefs = fs.readFileSync(filePath, 'utf-8');
+const resolvers = require('./resolvers');
+
+require('dotenv').config({ path: 'variables.env' });
+const User = require('./models/User');
+const Post = require('./models/Post');
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log(`DB Connected`))
+  .catch(err => console.error(err));
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: {
+    User,
+    Post,
+  },
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Server is listening on ${url}`);
+});
