4ed44bbc4f694141a6ffa5ce47fceb6c686d5140
Stephanie <stephaniiehallberg@gmail.com>
2019-06-21 05:56:06 +0200

    Vue - Added Plugin Vuetify and Added Navbar

--- server.js
+++ server.js
@@ -2,14 +2,18 @@ const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
 const fs = require('fs');
 const path = require('path');
+
+// import typeDefs and resolvers
 const filePath = path.join(__dirname, 'typeDefs.gql');
 const typeDefs = fs.readFileSync(filePath, 'utf-8');
 const resolvers = require('./resolvers');
 
+// import Environment variables and mongoose models 
 require('dotenv').config({ path: 'variables.env' });
 const User = require('./models/User');
 const Post = require('./models/Post');
 
+// connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI, {
     useCreateIndex: true,
@@ -18,6 +22,7 @@ mongoose
   .then(() => console.log(`DB Connected`))
   .catch(err => console.error(err));
 
+// create Apollo/graphQL server
 const server = new ApolloServer({
   typeDefs,
   resolvers,
