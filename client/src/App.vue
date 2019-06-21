44e45c66d9f9acde465369affbcbbda542125843
Stephanie <stephaniiehallberg@gmail.com>
2019-06-21 06:26:07 +0200

    Added SideNav and Routing/Pages

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,46 @@
 <template>
-  <v-app>
+  <v-app style="background: #E3E3EE">
+    <!-- Side Navbar -->
+    <v-navigation-drawer
+      app
+      tempory
+      fixed
+      v-model="sideNav"
+    >
+      <v-toolbar
+        color="accent"
+        dark
+        flat
+      >
+        <v-toolbar-side-icon @click="toggleSideNav"></v-toolbar-side-icon>
+        <router-link
+          to="/"
+          tag="span"
+          style="cursor: pointer"
+        >
+          <h1 class="title pl-3">VueShare</h1>
+        </router-link>
+
+      </v-toolbar>
+      <v-divider></v-divider>
+
+      <!-- Side Navbar Links -->
+      <v-list>
+        <v-list-tile
+          ripple
+          v-for="item in sideNavItems"
+          :key="item.title"
+          :to="item.link"
+        >
+          <v-list-tile-action>
+            <v-icon>{{item.icon}}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            {{item.title}}
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
     <!-- Horizontal Navbar -->
     <v-toolbar
       fixed
@@ -7,7 +48,7 @@
       dark
     >
       <!-- App title -->
-      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-side-icon @click="toggleSideNav"></v-toolbar-side-icon>
       <v-toolbar-title class="hidden-xs-only">
         <router-link
           to="/"
@@ -46,7 +87,10 @@
     <!-- App Content -->
     <main>
       <v-container class="mt-4">
-        <router-view />
+        <transition name="fade">
+          <router-view />
+
+        </transition>
       </v-container>
 
     </main>
@@ -56,6 +100,11 @@
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      sideNav: false
+    };
+  },
   computed: {
     horizontalNavItems() {
       return [
@@ -63,7 +112,37 @@ export default {
         { icon: "lock_open", title: "Sign In", link: "/signin" },
         { icon: "create", title: "Sign Up", link: "/signup" }
       ];
+    },
+    sideNavItems() {
+      return [
+        { icon: "chat", title: "Posts", link: "/posts" },
+        { icon: "lock_open", title: "Sign In", link: "/signin" },
+        { icon: "create", title: "Sign Up", link: "/signup" }
+      ];
+    }
+  },
+  methods: {
+    toggleSideNav() {
+      this.sideNav = !this.sideNav;
     }
   }
 };
 </script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition-property: opacity;
+  transition-duration: 0.25s;
+}
+
+.fade-enter-active {
+  transition-delay: 0.25s;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
+</style>
+
