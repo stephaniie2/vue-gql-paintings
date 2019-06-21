85d1b9fc284b48e1e56196248ae2df51bee8865c
Stephanie <stephaniiehallberg@gmail.com>
2019-06-21 08:06:56 +0200

    Using Store for GQL

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,12 +1,12 @@
 <template>
-  <v-container text-xs-center v-if="getPosts">
+  <v-container text-xs-center>
     <v-flex xs12>
       <v-carousel
         v-bind="{ 'cycle': true}"
         interval="3000"
       >
         <v-carousel-item
-          v-for="post in getPosts"
+          v-for="post in posts"
           :key="post._id"
           :src="post.imageUrl"
         >
@@ -14,7 +14,6 @@
         </v-carousel-item>
       </v-carousel>
     </v-flex>
-
   </v-container>
 </template>
 
@@ -22,19 +21,18 @@
 import { gql } from "apollo-boost";
 export default {
   name: "home",
-  apollo: {
-    getPosts: {
-      query: gql`
-        query {
-          getPosts {
-            _id
-            title
-            imageUrl
-            description
-            likes
-          }
-        }
-      `
+  created() {
+    this.handleGetCarouselPosts();
+  },
+  computed: {
+    posts() {
+      return this.$store.getters.posts;
+    }
+  },
+  methods: {
+    handleGetCarouselPosts() {
+      // reach out to Vuex store, fire action that gets posts
+      this.$store.dispatch("getPosts");
     }
   }
 };
--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,46 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+import { gql } from 'apollo-boost';
+import { defaultClient as ApolloClient } from './main';
+
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    posts: [],
   },
   mutations: {
-
+    setPosts: (state, payload) => {
+      state.posts = payload;
+    },
   },
   actions: {
-
+    getPosts: ({ commit }) => {
+      // use ApolloClient to fetch getPosts query
+      ApolloClient.query({
+        query: gql`
+          query {
+            getPosts {
+              _id
+              title
+              imageUrl
+            }
+          }
+        `,
+      })
+        .then(({ data }) => {
+          // Pass data to state via mutations
+          // commit passes data from actions along to mutation function
+          commit('setPosts', data.getPosts);
+          console.log(data.getPosts);
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    },
+  },
+  getters: {
+    posts: state => state.posts
   }
-})
+});
