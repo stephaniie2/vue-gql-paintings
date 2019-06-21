2edb0588a35a20610a8e5cff81e8645ef33ca1c2
Stephanie <stephaniiehallberg@gmail.com>
2019-06-21 09:10:14 +0200

    Server - SignIn and SignUp

--- client/src/queries.js
+++ client/src/queries.js
@@ -17,5 +17,18 @@ export const GET_POSTS = gql`
 /* Posts Mutation */
 
 /*User Mutations */
+export const SIGNIN_USER = gql`
+  mutation($username: String!, $password: String!) {
+    signinUser(username: $username, password: $password) {
+      token
+    }
+  }
+`;
 
-
+export const SIGNUP_USER = gql`
+  mutation($username: String!, $email: String!, $password: String!) {
+    signupUser(username: $username, email: $email, password: $password) {
+      token
+    }
+  }
+`;
