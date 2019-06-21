416b3a43a8e03b8a1b32df5df67348897bce5b22
Stephanie <stephaniiehallberg@gmail.com>
2019-06-21 08:16:32 +0200

    Add Loading Spinner

--- client/src/store.js
+++ client/src/store.js
@@ -9,14 +9,19 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     posts: [],
+    loading: false,
   },
   mutations: {
     setPosts: (state, payload) => {
       state.posts = payload;
     },
+    setLoading: (state, payload) => {
+      state.loading = payload;
+    }
   },
   actions: {
     getPosts: ({ commit }) => {
+      commit('setLoading', true);
       // use ApolloClient to fetch getPosts query
       ApolloClient.query({
         query: gql`
@@ -33,14 +38,17 @@ export default new Vuex.Store({
           // Pass data to state via mutations
           // commit passes data from actions along to mutation function
           commit('setPosts', data.getPosts);
+          commit('setLoading', false);
           console.log(data.getPosts);
         })
         .catch(err => {
+          commit('setLoading', false);
           console.error(err);
         });
     },
   },
   getters: {
-    posts: state => state.posts
-  }
+    posts: state => state.posts,
+    loading: state => state.loading
+  },
 });
