056df75d3bf07d3257af09e312354074a7c6af0b
stephaniie2 <stephaniiehallberg@gmail.com>
2019-06-22 18:57:49 +0200

    Add Grid Layout and Cards to Posts

--- client/src/components/Posts/Posts.vue
+++ client/src/components/Posts/Posts.vue
@@ -1,22 +1,90 @@
 <template>
   <v-container
-    text-xs-center
-    v-if="infiniteScrollPosts"
+    fluid
+    grid-list-xl
   >
-    <div
-      v-for="post in infiniteScrollPosts.posts"
-      :key="post._id"
+    <!-- Post Cards -->
+    <v-layout
+      row
+      wrap
+      v-if="infiniteScrollPosts"
     >
-      <img
-        :src="post.imageUrl"
-        height="100px"
+      <v-flex
+        xs12
+        sm6
+        v-for="post in infiniteScrollPosts.posts"
+        :key="post._id"
       >
-      <h3>{{post.title}}</h3>
-    </div>
-    <v-btn
-      @click="showMorePosts"
-      v-if="showMoreEnabled"
-    >Fetch More</v-btn>
+        <v-card hover>
+          <v-img
+            :src="post.imageUrl"
+            height="30vh"
+            lazy
+          ></v-img>
+          <v-card-actions>
+            <v-card-title primary>
+              <div>
+                <div class="headline">{{post.title}}</div>
+                <span class="grey--text">{{post.likes}} likes - {{post.message}} comments</span>
+              </div>
+            </v-card-title>
+            <v-spacer></v-spacer>
+            <v-btn
+              @click="showPostCreator = !showPostCreator"
+              icon
+            >
+              <v-icon>{{`keyboard_arrow_${showPostCreator ? 'up' : 'down'}`}}</v-icon>
+            </v-btn>
+          </v-card-actions>
+
+          <!-- Post Creator Tile -->
+          <v-slide-y-transition>
+            <v-card-text
+              v-show="showPostCreator"
+              class="grey lighten-4"
+            >
+              <v-list-tile avatar>
+                <v-list-tile-avatar>
+                  <img :src="post.createdBy.avatar">
+                </v-list-tile-avatar>
+                <v-list-tile-content>
+                  <v-list-tile-title class="text--primary">{{post.createdBy.username}}</v-list-tile-title>
+                  <v-list-tile-title class="font-weight-thin">
+                    Added {{post.createdDate}}
+                  </v-list-tile-title>
+                </v-list-tile-content>
+                <v-list-tile-action>
+                  <v-btn
+                    icon
+                    ripple
+                  >
+                    <v-icon color="grey lighten-1">info</v-icon>
+                  </v-btn>
+                </v-list-tile-action>
+              </v-list-tile>
+            </v-card-text>
+          </v-slide-y-transition>
+          <!-- End Post Creator Tile -->
+
+        </v-card>
+      </v-flex>
+    </v-layout>
+
+    <!-- Fetch More Button -->
+    <v-layout column>
+      <v-flex xs12>
+        <v-layout
+          justify-center
+          row
+        >
+          <v-btn
+            color="info"
+            @click="showMorePosts"
+          >Fetch More</v-btn>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+
   </v-container>
 </template>
 
@@ -30,7 +98,8 @@ export default {
   data() {
     return {
       pageNum: 1,
-      showMoreEnabled: true
+      showMoreEnabled: true,
+      showPostCreator: false
     };
   },
   apollo: {
