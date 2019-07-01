0699f1c54fe9ecef413736ec56f5a93610bb4d4e
Åke Hallberg <akehallberg@Akes-iMac.lan>
2019-07-01 16:02:56 +0200

    Post Message

--- client/src/components/Posts/Post.vue
+++ client/src/components/Posts/Post.vue
@@ -16,7 +16,12 @@
 
           <v-tooltip right>
             <span>Click to enlarge image</span>
-            <v-img @click="toggleImageDialog" slot="activator" :src="getPost.imageUrl" id="post__image"></v-img>
+            <v-img
+              @click="toggleImageDialog"
+              slot="activator"
+              :src="getPost.imageUrl"
+              id="post__image"
+            ></v-img>
           </v-tooltip>
 
           <!-- Post Imagee Dialog -->
@@ -35,11 +40,72 @@
         </v-card>
       </v-flex>
     </v-layout>
+
+    <!-- Messages Section -->
+    <div class="mt-3">
+      <!-- Message Input -->
+      <v-layout class="mb-3" v-if="user">
+        <v-flex xs12>
+          <v-form
+            v-model="isFormValid"
+            lazy-validation
+            ref="form"
+            @submit.prevent="handleAddPostMessage"
+          >
+            <v-layout row>
+              <v-flex xs12>
+                <v-text-field
+                  :rules="messageRules"
+                  v-model="messageBody"
+                  clearable
+                  :append-outer-icon="messageBody && 'send'"
+                  label="Add Message"
+                  type="text"
+                  @click:append-outer="handleAddPostMessage"
+                  prepend-icon="email"
+                  required
+                ></v-text-field>
+              </v-flex>
+            </v-layout>
+          </v-form>
+        </v-flex>
+      </v-layout>
+
+      <!-- Messages -->
+      <v-layout row wrap>
+        <v-flex xs12>
+          <v-list subheader two-line>
+            <v-subheader>Messages ({{getPost.messages.length}})</v-subheader>
+            <template v-for="message in getPost.messages">
+              <v-divider :key="message._id"></v-divider>
+              <v-list-tile avatar inset :key="message.title">
+                <v-list-tile-avatar>
+                  <img :src="message.messageUser.avatar" />
+                </v-list-tile-avatar>
+                <v-list-tile-content>
+                  <v-list-tile-title>{{message.messageBody}}</v-list-tile-title>
+                  <v-list-tile-sub-title>
+                    {{message.messageUser.username}}
+                    <span
+                      class="grey--text text--lighten-1 hidden-xs-only"
+                    >{{message.messageDate}}</span>
+                  </v-list-tile-sub-title>
+                </v-list-tile-content>
+
+                <v-list-tile-action class="hidden-xs-only">
+                  <v-icon :color="isOwnMessage(message) ? 'accent' : 'grey'">chat_bubble</v-icon>
+                </v-list-tile-action>
+              </v-list-tile>
+            </template>
+          </v-list>
+        </v-flex>
+      </v-layout>
+    </div>
   </v-container>
 </template>
 
 <script>
-import { GET_POST } from "../../queries";
+import { GET_POST, ADD_POST_MESSAGE, GET_POSTS } from "../../queries";
 import { mapGetters } from "vuex";
 
 export default {
@@ -47,8 +113,14 @@ export default {
   props: ["postId"],
   data() {
     return {
-      dialog: false
-    }
+      dialog: false,
+      messageBody: "",
+      isFormValid: true,
+      messageRules: [
+        message => !!message || "Message is required",
+        msg => msg.length < 50 || "Message must be less then 50 characters"
+      ]
+    };
   },
   apollo: {
     getPost: {
@@ -64,6 +136,39 @@ export default {
     ...mapGetters(["user"])
   },
   methods: {
+    handleAddPostMessage() {
+      if (this.$refs.form.validate()) {
+        const variables = {
+          messageBody: this.messageBody,
+          userId: this.user._id,
+          postId: this.postId
+        };
+        this.$apollo
+          .mutate({
+            mutation: ADD_POST_MESSAGE,
+            variables,
+            update: (cache, { data: { addPostMessage } }) => {
+              const data = cache.readQuery({
+                query: GET_POST,
+                variables: {
+                  postId: this.postId
+                }
+              });
+              data.getPost.messages.unshift(addPostMessage);
+              cache.writeQuery({
+                query: GET_POST,
+                variables: { postId: this.postId },
+                data
+              });
+            }
+          })
+          .then(({ data }) => {
+            this.$refs.form.reset();
+            console.log(data.addPostMessage);
+          })
+          .catch(err => console.errror(err));
+      }
+    },
     goToPrevPage() {
       this.$router.go(-1);
     },
@@ -71,6 +176,9 @@ export default {
       if (window.innerWidth > 500) {
         this.dialog = !this.dialog;
       }
+    },
+    isOwnMessage(message) {
+      return this.user && this.user._id === message.messageUser._id;
     }
   }
 };
--- client/src/components/Posts/Posts.vue
+++ client/src/components/Posts/Posts.vue
@@ -3,8 +3,8 @@
     <!-- Post Cards -->
     <v-layout row wrap v-if="infiniteScrollPosts">
       <v-flex xs12 sm6 v-for="post in infiniteScrollPosts.posts" :key="post._id">
-        <v-card @click.native="goToPost(post._id)" hover>
-          <v-img :src="post.imageUrl" height="30vh" lazy></v-img>
+        <v-card hover>
+          <v-img @click.native="goToPost(post._id)" :src="post.imageUrl" height="30vh" lazy></v-img>
           <v-card-actions>
             <v-card-title primary>
               <div>
